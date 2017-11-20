266b7b7a19c8794e747004f3b53248caa245f1d6
Maciej Ostrowski <maciek@zmyslni.pl>
2017-11-20 10:49:51 +0100

    Better error messages

--- dca/tl_content.php
+++ dca/tl_content.php
@@ -141,7 +141,7 @@ class tl_content_cte_wrapper extends Backend
                 }
 
                 if (count($openedTagsStack) === 0) {
-                    $status[$statusTitle] = '<span class="tl_red">Error: Closing tag "' . $closingTags[0]['tag'] . '" (id:' . $cte->id . ') is without opening tag.</span>';
+                    $status[$statusTitle] = '<span class="tl_red">Error: Closing tag "&lt;/' . $closingTags[0]['tag'] . '&gt;" (id:' . $cte->id . ') is without opening tag.</span>';
                     break;
                 }
 
@@ -150,12 +150,12 @@ class tl_content_cte_wrapper extends Backend
                     $openedTag = array_pop($openedTagsStack);
 
                     if ($openedTag === null) {
-                        $status[$statusTitle] = '<span class="tl_red">Error: Closing tag "' . $closingTag['tag'] . '" (id:' . $cte->id . ') is without opening tag.</span>';
+                        $status[$statusTitle] = '<span class="tl_red">Error: Closing tag &lt;/"' . $closingTag['tag'] . '&gt;" (id:' . $cte->id . ') is without opening tag.</span>';
                         break 2;
                     }
 
                     if ($closingTag['tag'] !== $openedTag[1]['tag']) {
-                        $status[$statusTitle] = '<span class="tl_red">Error: Opening tag "' . $openedTag[1]['tag'] . '"  (id:' . $openedTag[0] . ') is paired with closing tag "' . $closingTag['tag'] . '" (id:' . $cte->id . ').</span>';
+                        $status[$statusTitle] = '<span class="tl_red">Error: Opening tag "&lt;' . $openedTag[1]['tag'] . '&gt;"  (id:' . $openedTag[0] . ') is paired with closing tag "&lt;/' . $closingTag['tag'] . '&gt;" (id:' . $cte->id . ').</span>';
                         break 2;
                     }
                 }
@@ -165,7 +165,7 @@ class tl_content_cte_wrapper extends Backend
         if (count($status) === 0) {
             if (count($openedTagsStack)) {
                 $openedTag = array_pop($openedTagsStack);
-                $status[$statusTitle] = '<span class="tl_red">Error: Opening tag "' . $openedTag[1]['tag'] . '" (id:' . $openedTag[0] . ') is without closing tag.</span>';
+                $status[$statusTitle] = '<span class="tl_red">Error: Opening tag "&lt;' . $openedTag[1]['tag'] . '&gt;" (id:' . $openedTag[0] . ') is without closing tag.</span>';
             } else {
                 $status[$statusTitle] = 'Opening tags match closing tags';
             }
