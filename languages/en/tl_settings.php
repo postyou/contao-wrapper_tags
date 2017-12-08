8922e52644fea2dd13e38610bee99d509bacd1ba
Maciej Ostrowski <maciek@zmyslni.pl>
2017-12-08 14:20:32 +0100

    better and missing translation

--- languages/en/tl_settings.php
+++ languages/en/tl_settings.php
@@ -10,6 +10,6 @@
 
 $GLOBALS['TL_LANG']['tl_settings']['wrapper-tags_legend'] = 'Wrapper tags settings';
 $GLOBALS['TL_LANG']['tl_settings']['wrapperTagsAllowedTags'] = array('HTML tags for selection', 'Here you can enter a list of HTML tags used in Wrapper tags elements.');
-$GLOBALS['TL_LANG']['tl_settings']['wrapperTagsUseColors'] = array('Colorize wrapper tags', 'Check if wrapper elements indented more then 2 time should be colorized.');
+$GLOBALS['TL_LANG']['tl_settings']['wrapperTagsUseColors'] = array('Colorize wrapper tags', 'Check whether wrapper elements indented by more then 2 levels should be colorized.');
 $GLOBALS['TL_LANG']['tl_settings']['wrapperTagsHideValidationStatus'] = array('Hide validation status', 'Check to hide validation result status.');
 
--- languages/pl/tl_settings.php
+++ languages/pl/tl_settings.php
@@ -11,5 +11,6 @@
 
 $GLOBALS['TL_LANG']['tl_settings']['wrapper-tags_legend'] = 'Ustawienia HTML-owych znaczników';
 $GLOBALS['TL_LANG']['tl_settings']['wrapperTagsAllowedTags'] = array('Dostępne znaczniki', 'Wpisz znaczniki, które będą dostępne do wyboru w rozwijanej liście.');
-$GLOBALS['TL_LANG']['tl_settings']['wrapperTagsUseColors'] = array('Pokolorować na liście w artykule?', 'Zaznacz, jeśli elementy treści znaczników widoczne na liście w artykule i wcięte więcej niż 1 poziom, mają być pokolorowane.');
+$GLOBALS['TL_LANG']['tl_settings']['wrapperTagsUseColors'] = array('Pokoloruj elementy', 'Zaznacz jeśli elementy znaczników na liście elementów i wcięte więcej niż o 1 poziom mają być pokolorowane.');
+$GLOBALS['TL_LANG']['tl_settings']['wrapperTagsHideValidationStatus'] = array('Ukryj status walidacji', 'Zaznacz aby ukryć status wyniku walidacji struktury html-a.');
 
