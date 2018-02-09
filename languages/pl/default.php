41a7b341a65eeef25b6ce392dfe0c8073af8d286
Maciej Ostrowski <maciek@zmyslni.pl>
2018-02-09 11:19:44 +0100

    First PL translation

--- languages/pl/default.php
+++ languages/pl/default.php
@@ -1,6 +1,6 @@
 <?php
 
-/**
+/*
  * Copyright (C) 2018 Zmyslni
  *
  * @author  Ostrowski Maciej <http://contao-developer.pl>
@@ -8,7 +8,46 @@
  * @license LGPL-3.0+
  */
 
+/*
+ * CTE labels
+ */
+$GLOBALS['TL_LANG']['CTE']['wt_opening_tags'] = array('Otwieranie znaczników', 'Dodaje wiele otwarć znaczników do strony.');
+$GLOBALS['TL_LANG']['CTE']['wt_closing_tags'] = array('Domykanie znaczników', 'Dodaje wiele domknięć znaczników do strony.');
+$GLOBALS['TL_LANG']['CTE']['wrapper_tags'] = 'HTML-owe znaczniki';
 
-$GLOBALS['TL_LANG']['CTE']['openingTags'] = array('Otwieranie znaczników', 'Pozwala na otwarcie wielu znaczników html-a.');
-$GLOBALS['TL_LANG']['CTE']['closingTags'] = array('Domykanie znaczników', 'Pozwala na domknięcie wielu znaczników html-a.');
-$GLOBALS['TL_LANG']['CTE']['wrapperTags'] = 'HTML-owe znaczniki';
+/*
+ * Wrapper tags structure validation statuses
+ */
+$GLOBALS['TL_LANG']['MSC']['wt.dataCorrupted'] = 'Corrupted data';
+$GLOBALS['TL_LANG']['MSC']['wt.statusTitle'] = 'Wrapper tags';
+$GLOBALS['TL_LANG']['MSC']['wt.statusOk'] = 'ok';
+$GLOBALS['TL_LANG']['MSC']['wt.validationError'] = 'Validation error';
+$GLOBALS['TL_LANG']['MSC']['wt.statusClosingNoOpening'] = 'Error: Closing tag "&lt;/%s&gt;" (id:%d) is without opening tag.';
+$GLOBALS['TL_LANG']['MSC']['wt.statusOpeningNoClosing'] = 'Error: Opening tag "&lt;%s&gt;" (id:%d) is without closing tag.';
+$GLOBALS['TL_LANG']['MSC']['wt.statusOpeningWrongPairing'] = 'Error: Opening tag "&lt;%s&gt;" (id:%d) is paired with closing tag "&lt;/%s&gt;" (id:%d).';
+$GLOBALS['TL_LANG']['MSC']['wt.statusOpeningWrongPairingWithOther'] = 'Error: Opening tag "&lt;%s&gt;" (id:%d) is paired with wrong closing element "%s" (id:%d).';
+$GLOBALS['TL_LANG']['MSC']['wt.statusClosingWrongPairingWithOther'] = 'Error: Closing tag "&lt;%s&gt;" (id:%d) is paired with wrong opening element "%s" (id:%d).';
+$GLOBALS['TL_LANG']['MSC']['wt.statusClosingWrongPairingNeedSplit'] = 'Error: Closing tags (id:%d) is paired with many smaller opening tags. First one is to big (id:%d).';
+
+/*
+ * Opening tags widget validation errors
+ */
+$GLOBALS['TL_LANG']['MSC']['wt.errorAttributeNameAlreadyUsed'] = 'The attribute name "%s" used more then once';
+$GLOBALS['TL_LANG']['MSC']['wt.errorAttributeNameWithoutValue'] = 'The attribute name "%s" is without a value';
+$GLOBALS['TL_LANG']['MSC']['wt.errorAttributeValueWithoutName'] = 'The attribute value "%s" is without a name';
+$GLOBALS['TL_LANG']['MSC']['wt.errorAttributeName'] = 'The attribute name "%s" has the wrong semantic. It must begin with a letter ([A-Za-z]) and may be followed by any number of letters, digits ([0-9]), hyphens ("-"), underscores ("_"), colons (":"), and periods ("."). It can also contain Contao insert tags.';
+
+
+/**
+ * Wrapper-tags status messages
+ */
+//$GLOBALS['TL_LANG']['MSC']['wt.statusTitle'] = 'Struktura';
+//$GLOBALS['TL_LANG']['MSC']['wt.statusOk'] = 'poprawna';
+//$GLOBALS['TL_LANG']['MSC']['wt.dataCorrupted'] = 'Dane są uszkodzone';
+//$GLOBALS['TL_LANG']['MSC']['wt.validationError'] = 'Błąd walidacji';
+//$GLOBALS['TL_LANG']['MSC']['wt.statusClosingNoOpening'] = 'Błąd: Domknięcie znacznika "&lt;/%s&gt;" (id:%d) nie ma znacznika otwierającego.';
+//$GLOBALS['TL_LANG']['MSC']['wt.statusOpeningNoClosing'] = 'Błąd: Otwarcie znacznika "&lt;%s&gt;" (id:%d) nie ma domknięcia.';
+//$GLOBALS['TL_LANG']['MSC']['wt.statusOpeningWrongPairing'] = 'Błąd: Otwarcie znacznika "&lt;%s&gt;" (id:%d) jest sparowane z domknięciem "&lt;/%s&gt;" (id:%d)';
+//$GLOBALS['TL_LANG']['MSC']['wt.statusOpeningWrongPairingWithOther'] = 'Błąd: Otwarcie znacznika "&lt;%s&gt;" (id:%d) jest sparowane z niewłaściwym elementem domykającym "%s" (id:%d).';
+//$GLOBALS['TL_LANG']['MSC']['wt.statusClosingWrongPairingWithOther'] = 'Błąd: Domknięcie znacznika "&lt;%s&gt;" (id:%d) jest sparowane z niewłaściwym elementem otwierającym "%s" (id:%d).';
+//$GLOBALS['TL_LANG']['MSC']['wt.statusClosingWrongPairingNeedSplit'] = 'Błąd: Domknięcie znacznika (id:%d) jest sparowane z wieloma mniejszymi otwarciami. Pierwsze z nich jest za duże (id:%d).';
--- languages/pl/tl_content.php
+++ languages/pl/tl_content.php
@@ -8,24 +8,22 @@
  * @license LGPL-3.0+
  */
 
-
-$GLOBALS['TL_LANG']['tl_content']['wt_opening_tags'] = array('Otwieranie znaczników', 'Wybierz znaczniki do otwarcia. W odpowiadającym elemencie treści [Domykanie znaczników] ta struktura musi być odwzorowana. Każdy otwarty znacznik np. &lt;DIV&gt; musi mieć domknięty znacznik &lt;/DIV&gt;');
-$GLOBALS['TL_LANG']['tl_content']['wt_closing_tags'] = array('Domykanie znaczników', 'Wybierz te znaczniki, które znajdują się w odpowiadającym elemencie treści [Otwieranie znaczników].');
-$GLOBALS['TL_LANG']['tl_content']['wt_tag'] = array('Znacznik', 'Wybierz znacznik');
-$GLOBALS['TL_LANG']['tl_content']['wt_class'] = array('Class', 'Wpisz klasy');
-$GLOBALS['TL_LANG']['tl_content']['wrappertags_legend'] = 'Ustawienia HTML-owych znaczników';
-
-
-/**
- * Wrapper-tags status messages
+/*
+ * Fields
  */
-$GLOBALS['TL_LANG']['MSC']['wt.statusTitle'] = 'Struktura';
-$GLOBALS['TL_LANG']['MSC']['wt.statusOk'] = 'poprawna';
-$GLOBALS['TL_LANG']['MSC']['wt.dataCorrupted'] = 'Dane są uszkodzone';
-$GLOBALS['TL_LANG']['MSC']['wt.validationError'] = 'Błąd walidacji';
-$GLOBALS['TL_LANG']['MSC']['wt.statusClosingNoOpening'] = 'Błąd: Domknięcie znacznika "&lt;/%s&gt;" (id:%d) nie ma znacznika otwierającego.';
-$GLOBALS['TL_LANG']['MSC']['wt.statusOpeningNoClosing'] = 'Błąd: Otwarcie znacznika "&lt;%s&gt;" (id:%d) nie ma domknięcia.';
-$GLOBALS['TL_LANG']['MSC']['wt.statusOpeningWrongPairing'] = 'Błąd: Otwarcie znacznika "&lt;%s&gt;" (id:%d) jest sparowane z domknięciem "&lt;/%s&gt;" (id:%d)';
-$GLOBALS['TL_LANG']['MSC']['wt.statusOpeningWrongPairingWithOther'] = 'Błąd: Otwarcie znacznika "&lt;%s&gt;" (id:%d) jest sparowane z niewłaściwym elementem domykającym "%s" (id:%d).';
-$GLOBALS['TL_LANG']['MSC']['wt.statusClosingWrongPairingWithOther'] = 'Błąd: Domknięcie znacznika "&lt;%s&gt;" (id:%d) jest sparowane z niewłaściwym elementem otwierającym "%s" (id:%d).';
-$GLOBALS['TL_LANG']['MSC']['wt.statusClosingWrongPairingNeedSplit'] = 'Błąd: Domknięcie znacznika (id:%d) jest sparowane z wieloma mniejszymi otwarciami. Pierwsze z nich jest za duże (id:%d).';
+$GLOBALS['TL_LANG']['tl_content']['wt_opening_tags'] = array('Otwarcia znaczników', 'Wybierz znaczniki i ustaw ich atrybuty.');
+$GLOBALS['TL_LANG']['tl_content']['wt_closing_tags'] = array('Domknięcia znaczników', 'Wybierz znaczniki.');
+
+/*
+ * MultiColumnWizard fields
+ */
+$GLOBALS['TL_LANG']['tl_content']['wt_tag'] = array('Znacznik', 'Wybierz znacznik.');
+$GLOBALS['TL_LANG']['tl_content']['wt_class'] = array('Klasa', 'Tutaj możesz wpisać klasy css.');
+$GLOBALS['TL_LANG']['tl_content']['wt_attribute'] = array('Inne atrybuty [Nazwa - Wartość]', 'Tutaj możesz wprowadzić dowolne atrybuty: Nazwa + Wartość');
+$GLOBALS['TL_LANG']['tl_content']['wt_attribute_name'] = 'Nazwa';
+$GLOBALS['TL_LANG']['tl_content']['wt_attribute_value'] = 'Wartość';
+
+/*
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_content']['wt_legend'] = 'HTML-owe znaczniki';
