9c7226c46e067ef911563dfceabc63e1663f12f9
TheDyingMountain <UwkyzPPxDfyCH11NMTW3>
2023-11-30 11:33:49 +0100

    add bundle file + plugin php

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of Postyou Wrapper Tags.
+ *
+ * @package     contao-wrapper_tags
+ * @license     AGPL-3.0
+ * @author      Mario Gienapp <https://github.com/thedyingmountain>
+ * @copyright   Postyou <https://www.postyou.de/>
+ */
+declare(strict_types=1);
+
+namespace Postyou\WrapperTags\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Postyou\WrapperTags\ContaoWrapperTags;
+
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(ContaoWrapperTags::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+                ->setReplace(['wrapper_tags']),
+        ];
+    }
+
+}
---
+++ src/ContaoWrapperTags.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of Postyou Wrapper Tags.
+ *
+ * @package     contao-wrapper_tags
+ * @license     AGPL-3.0
+ * @author      Mario Gienapp <https://github.com/thedyingmountain>
+ * @copyright   Postyou <https://www.postyou.de/>
+ */
+
+declare(strict_types=1);
+
+namespace Postyou\WrapperTags;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ContaoWrapperTags extends Bundle
+{
+}
