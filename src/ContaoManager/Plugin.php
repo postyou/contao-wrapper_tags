5e25d6af959a9902195caf64a0abe496b8121076
TheDyingMountain <UwkyzPPxDfyCH11NMTW3>
2023-12-04 14:05:59 +0100

    remove underscore

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -15,7 +15,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Postyou\ContaoWrapper_Tags\ContaoWrapperTags;
+use Postyou\ContaoWrapperTags\ContaoWrapperTags;
 
 
 class Plugin implements BundlePluginInterface
